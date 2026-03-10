11615005a2c83d314e6368f51388dfe37d2a5836
Mark Reyes <rmark492@outlook.com>
2026-03-11 07:48:40 +0800

    remove unnecessary code in backend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,14 +10,7 @@ Route::prefix('auth')->group(function() {
     Route::post('/register', [AuthController::class, 'register']);
 });
 
-Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
-    return response()->json([
-        "success" => true,
-        "user"  => $request->user()
-    ]);
-});
-
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/me', [AuthController::class, 'me']);
+    Route::get('/me', [AuthController::class, 'me']);
     Route::post('auth/logout', [AuthController::class, 'logout']);
 });
